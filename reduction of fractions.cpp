1aaa149c5d53b997302edc925e0583fef6bbd3f3
Cheprichate <petrovich15071998@mail.ru>
2023-10-10 11:12:31 +0400

    Добавьте файлы проекта.

---
+++ reduction of fractions.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+int lower = 0;
+int height = 63;
+int middle;
+char ans;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    while (lower < height)
+    {
+        middle = (height + lower) / 2;
+        do
+        {
+            std::cout << "Число меньше или равно " << middle << " ? (y/n)";
+            std::cin >> ans;
+        } while (ans != 'y' && ans != 'n');
+
+        if (ans == 'y')
+            height = middle;
+        else
+            lower = middle + 1;
+        std::cout << "(" << lower << " : " << height << ")\n";
+    }
+
+    std::cout << "Это ваш номер  " << height << "\n";
+}
